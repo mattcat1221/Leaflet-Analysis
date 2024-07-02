fdc786b37ce4688e6675d9d01d312687c642e776
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-07-02 17:00:03 -0400

    working on step one

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -6,7 +6,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker with a popup
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A sample marker with popup')
-    .openPopup();
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
+
+d3.json(url, data => {
+    console.log(data);
+
+    L.geoJSON(data, {
+        pointToLayer: (geoJsonPoint, latlng) => {
+            return L.circleMarker(latlng);
+        },
+        style: feature => {
+            let depth = feature.geometry.coordinates[2];
+
+            console.log();
+            return {
+                color: 'black',
+                weight: 1,
+                fillOpacity: .65,
+                fillColor: 
+                    depth > 90 ? 'red' : 
+                    depth > 70 ? 'darkorange' : 
+                    depth > 50 ? 'orange' : 
+                    depth > 30 ? 'yellow' :
+                    depth > 10 ? 'lime' : 'green'
+            };
+        }
+    }).bindPopup(function (layer) {
+        return layer.feature.properties.description;
+    }).addTo(map);
+
+});
