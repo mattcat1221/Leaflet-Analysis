54e829da52859d8d57c2ee2ec7f984309c041924
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-07-03 16:48:27 -0400

    added bindpop and magnitue to the circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,12 +1,10 @@
-// Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 2);
+var map = L.map('map').setView([0, 0], 2);
 
-// Add a tile layer (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
+const url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
 
 d3.json(url, data => {
     console.log(data);
@@ -17,22 +15,32 @@ d3.json(url, data => {
         },
         style: feature => {
             let depth = feature.geometry.coordinates[2];
+            let mag = feature.properties.mag;
 
-            console.log();
+            console.log (feature.properties.mag)
+
+
             return {
+                radius: mag*2,
                 color: 'black',
                 weight: 1,
                 fillOpacity: .65,
                 fillColor: 
-                    depth > 90 ? 'red' : 
-                    depth > 70 ? 'darkorange' : 
-                    depth > 50 ? 'orange' : 
+                    depth > 90 ? 'red' :
+                    depth > 70 ? 'darkorange' :
+                    depth > 50 ? 'orange' :
                     depth > 30 ? 'yellow' :
                     depth > 10 ? 'lime' : 'green'
             };
         }
-    }).bindPopup(function (layer) {
-        return layer.feature.properties.description;
-    }).addTo(map);
+    }).bindPopup(layer => {
+        let depth = layer.feature.geometry.coordinates[2];
+        let place = layer.feature.properties.place;
+        let time = new Date(layer.feature.properties.time).toLocaleString();
+        let magnitude = layer.feature.properties.mag;
 
+        return `<h3>${place}<br>Magnitude: ${magnitude}<br>Depth: ${depth}<br>${time}</h3>`;
+
+
+    }).addTo(map);
 });
--- Leaflet-Step-1/static/js/logic2.js
+++ Leaflet-Step-1/static/js/logic2.js
@@ -8,12 +8,12 @@ var myMap = L.map("map", {
   
   // Adding a tile layer (the background map image) to our map
   // We use the addTo method to add objects to our map
-  // L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  //   maxZoom: 18,
-  //   id: "mapbox.streets",
-  //   accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
-  // }).addTo(myMap);
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
+  }).addTo(myMap);
 
   // Store our API endpoint
 // var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
